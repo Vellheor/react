d63aa4f1f4f79f25f9e4f39212c795839e717678
Vellheor <djdostoevskiy@gmail.com>
2020-04-30 01:29:55 +0300

    Lesson 51 axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import style from './users.module.css';
+import * as Axios from 'axios';
+import noPhoto from '../../assets/img/user.png';
 
 let Users = (props) => {
    if(props.users.length === 0){
-      props.setUsers([
-         {id:1, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: false, fullName: "Dmitry", status: "I am a manager", location: {city: 'Minsk', country: 'Belarus'}},
-         {id:2, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: true, fullName: "Alex", status: "I am a boss", location: {city: 'Toronto', country: 'Kanada'}},
-         {id:3, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: true, fullName: "Vera", status: "I am a wife", location: {city: 'Kiev', country: 'Ukraine'}},
-         {id:4, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: false, fullName: "Kusha", status: "I am a paradaise", location: {city: 'Borodyanka', country: 'Ukraine'}}
-      ])
+
+      Axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+         props.setUsers(response.data.items)
+      });
+
+
+      // props.setUsers([
+      //    {id:1, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: false, fullName: "Dmitry", status: "I am a manager", location: {city: 'Minsk', country: 'Belarus'}},
+      //    {id:2, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: true, fullName: "Alex", status: "I am a boss", location: {city: 'Toronto', country: 'Kanada'}},
+      //    {id:3, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: true, fullName: "Vera", status: "I am a wife", location: {city: 'Kiev', country: 'Ukraine'}},
+      //    {id:4, photoUrl: "https://ericson-lab.com/wp-content/uploads/2017/08/nophoto.png", followed: false, fullName: "Kusha", status: "I am a paradaise", location: {city: 'Borodyanka', country: 'Ukraine'}}
+      // ])
    }
    return (
    <div>
@@ -16,7 +24,7 @@ let Users = (props) => {
          props.users.map( u => <div key={u.id} className={style.grid_4}>
             <div>
                <div>
-                  <img src={u.photoUrl} className={style.photoUrl}/>
+                  <img src={u.photos.small != null ? u.photos.small : noPhoto} className={style.photoUrl}/>
                </div>
                <div>
                   {
@@ -27,8 +35,8 @@ let Users = (props) => {
                </div>
             </div>
             <div className={style.grid_3}>
-               <div><p>{u.fullName}</p><p>{u.status}</p></div>
-               <div><p>{u.location.country}</p><p>{u.location.city}</p></div>
+               <div><p>{u.name}</p><p>{u.status}</p></div>
+               <div><p>{"u.location.country"}</p><p>{"u.location.city"}</p></div>
             </div>
          </div>)
       }
