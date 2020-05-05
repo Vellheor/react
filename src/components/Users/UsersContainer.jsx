141fad535abfd919b00b7b9f1a8f60b25cac7396
Vellheor <djdostoevskiy@gmail.com>
2020-05-06 00:38:07 +0300

    Lesson 57

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Users from './Users';
-import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTatolUsersCountAC } from '../../redux/users-reducer';
+import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTatolUsersCountAC, setIsFetchingAC } from '../../redux/users-reducer';
 import Axios from 'axios';
+import Loader from '../common/Loader/Loader';
+
 
 
 class UsersApiComponent extends React.Component{
    componentDidMount(){
+      this.props.setIsFetching(true);
       Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+         this.props.setIsFetching(false);
          this.props.setUsers(response.data.items);
          this.props.setTatolUsersCount(response.data.totalCount);
       });
    }
    onPageChanged = (pageNumber) => {
+      this.props.setIsFetching(true);
       this.props.setCurrentPage(pageNumber);
       Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+         this.props.setIsFetching(false);
          this.props.setUsers(response.data.items)
       });
    }
    render(){
-    return <Users 
-    tatolUsersCount = {this.props.tatolUsersCount}
-    pageSize = {this.props.pageSize}
-    currentPage = {this.props.currentPage}
-    onPageChanged = {this.onPageChanged}
-    users = {this.props.users}
-    unfollow = {this.props.unfollow}
-    follow = {this.props.follow}
-    />
+    return <>
+      {this.props.isFetching ? <Loader /> : null}
+      <Users 
+      tatolUsersCount = {this.props.tatolUsersCount}
+      pageSize = {this.props.pageSize}
+      currentPage = {this.props.currentPage}
+      onPageChanged = {this.onPageChanged}
+      users = {this.props.users}
+      unfollow = {this.props.unfollow}
+      follow = {this.props.follow}
+      />
+   </>
    }
 }
 
@@ -39,7 +48,8 @@ let mapStateToProps = (state) => {
       users: state.usersPage.users,
       pageSize: state.usersPage.pageSize,
       tatolUsersCount: state.usersPage.tatolUsersCount,
-      currentPage: state.usersPage.currentPage
+      currentPage: state.usersPage.currentPage,
+      isFetching: state.usersPage.isFetching
    }
 }
 
@@ -59,6 +69,9 @@ let mapDispatchToProps = (dispatch) => {
       },
       setTatolUsersCount: (tatolUsersCount) => {
          dispatch(setTatolUsersCountAC(tatolUsersCount));
+      },
+      setIsFetching: (isFetching) => {
+         dispatch(setIsFetchingAC(isFetching));
       }
    }
 }
---
+++ src/components/common/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import loader from '../../../assets/img/loader.gif';
+
+let Loader = (props) => {
+   return <div><img src={loader}/></div>
+}
+
+export default Loader;
