5183f8f1787b23267f89123187ebfc919a60aba6
Vellheor <djdostoevskiy@gmail.com>
2020-05-07 02:54:57 +0300

    Lesson 62

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './users.module.css';
 import noPhoto from '../../assets/img/user.png';
 import { NavLink } from 'react-router-dom';
+import * as Axios from 'axios';
 
 
 let Users = (props) => {
@@ -32,8 +33,24 @@ let Users = (props) => {
                   <div>
                      {
                         u.followed 
-                           ? <button onClick={ () => { props.unfollow(u.id) } }>Unfollow</button> 
-                           : <button onClick={ () => { props.follow(u.id) } }>Follow</button>
+                           ? <button onClick={ () => {
+                                 Axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/`+u.id, { withCredentials: true, headers: {"API-KEY" : "82cdae22-32fe-4d5e-9535-92906ed357d0"} })
+                                 .then(response => {
+                                    if(response.data.resultCode === 0){
+                                       props.unfollow(u.id)
+                                    }
+                                 });
+                              } 
+                           }>Unfollow</button> 
+                           : <button onClick={ () => {
+                              Axios.post(`https://social-network.samuraijs.com/api/1.0/follow/`+u.id, {}, { withCredentials: true, headers: {"API-KEY" : "82cdae22-32fe-4d5e-9535-92906ed357d0"} })
+                                 .then(response => {
+                                    if(response.data.resultCode === 0){
+                                       props.follow(u.id)
+                                    }
+                                 }); 
+                               } 
+                           }>Follow</button>
                      }
                   </div>
                </div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,7 +10,9 @@ import Loader from '../common/Loader/Loader';
 class UsersApiComponent extends React.Component{
    componentDidMount(){
       this.props.setIsFetching(true);
-      Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+      Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
+         withCredentials: true
+      }).then(response => {
          this.props.setIsFetching(false);
          this.props.setUsers(response.data.items);
          this.props.setTatolUsersCount(response.data.totalCount);
@@ -19,7 +21,9 @@ class UsersApiComponent extends React.Component{
    onPageChanged = (pageNumber) => {
       this.props.setIsFetching(true);
       this.props.setCurrentPage(pageNumber);
-      Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+      Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{
+         withCredentials: true
+      }).then(response => {
          this.props.setIsFetching(false);
          this.props.setUsers(response.data.items)
       });
