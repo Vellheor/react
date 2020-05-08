3ed3f52b9bf4cc5381950b717a7927332c5819ac
Vellheor <djdostoevskiy@gmail.com>
2020-05-08 23:02:09 +0300

    Lesson 70 compose

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { userProfile } from '../../redux/profile-reducer';
 import { withRouter } from 'react-router-dom';
 import {withAuthRedirect} from '../hoc/withAuthRedirect';
+import { compose } from 'redux';
 
 class ProfileContainer extends React.Component{
    
@@ -24,14 +25,14 @@ class ProfileContainer extends React.Component{
    }
 }
 
-let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
-
 let mapStateToProps = (state) => (
    {
       profile: state.profilePage.profile
    }
 )
 
-let ProfileContainerWithRouterComponent = withRouter(AuthRedirectComponent);
-
-export default connect(mapStateToProps, {userProfile})(ProfileContainerWithRouterComponent);
+export default compose(
+   connect(mapStateToProps, {userProfile}),
+   withRouter,
+   withAuthRedirect
+)(ProfileContainer);
